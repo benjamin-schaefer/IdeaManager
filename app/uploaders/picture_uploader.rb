70046ee4a09980802eb55c12cc63dbbb6f4ec2cb
Benjamin Schaefer <benjamin.schaefer@werbeboten.de>
2014-09-22 10:49:38 +0200

    - image scale before upload (to 150x150 px) - adapted language file for errors at wrong login - adapted password form for login instead of email - deleted 2 images

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -25,22 +25,22 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
+  process resize_to_fit: [150, 150]
+  
   # def scale(width, height)
-  #   # do something
+  #   process resize_to_fill: [width, height]
   # end
 
   # Create different versions of your uploaded files:
-   version :thumb do
-     process resize_to_fill: [50, 50]
-   end
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-   def extension_white_list
-     %w(jpg jpeg gif png)
-   end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
